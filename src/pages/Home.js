8f32e0cf149ab2c8599bb5a85e103d0357b215cc
jinta1139 <109164144+jinta1139@users.noreply.github.com>
2023-09-19 01:06:13 -0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { Container, Row, Col } from "reactstrap";
 
 import { Link } from "react-router-dom";
 
-import guyImg from "../assets/images/delivery-guy.png";
+import heroImg from "../assets/images/hero.png";
 import "../styles/hero-section.css";
 
 const Home = () => {
@@ -16,21 +16,30 @@ const Home = () => {
             <Col lg="6" md="6">
               <div className="hero__content">
                 <h5 className="mb-3">Easy order & fast delivery</h5>
-                <h1 className="mb-4 hero__title">
-                  <span>Enjoy</span> your favorite Pizza
+                <h1 className="hero_title mb-4">
+                  <span>HUNGRY?</span> just wait <br /> food at
+                   <span> your door</span>
                 </h1>
+                
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Deserunt doloribus sed, asperiores ipsum repellendus natus.
 
-                <button className="order__btn d-flex align-items-center justify-content-between ">
-                  <Link to="/pizzas">
-                    Menu <i className="ri-arrow-right-s-line"></i>
-                  </Link>
+                <div className="hero_btns d-flex align-items-center gap-5 mt-4">
+                  <button className="order_btn d-flex align-items-center justify-content-between ">
+                    Order now <i className="ri-arrow-right-s-line"></i>
                 </button>
+                
+                <button className="all_foods-btn">
+                    <Link to="/foods">See all foods</Link>
+                </button>
+                </div>
+                
               </div>
             </Col>
 
             <Col lg="6" md="6">
               <div className="hero__img">
-                <img src={guyImg} alt="delivery-guy" className="w-100" />
+                <img src={heroImg} alt="hero-img" className="w-100" />
               </div>
             </Col>
           </Row>
